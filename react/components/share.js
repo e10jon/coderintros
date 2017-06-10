6e9cf2a81147c2cf3db6bec892b4abf553f1bcc7
Ethan Jon <ethan@Ethans-MacBook-Pro.local>
2017-06-10 13:56:46 -0400

    share

--- react/components/share.js
+++ react/components/share.js
@@ -5,14 +5,14 @@ import React from 'react'
 const Share = ({title, url}: Object) => {
   const encodedUrl = encodeURI(url)
   const encodedTitle = encodeURI(title)
-  const className = 'white inline-block py1 px2 h6 mb1 ups bold'
+  const className = 'border inline-block py1 px2 h6 mb1 mr1 ups bold'
 
   return (
     <div className='my3'>
       <a
         className={className}
         href={`https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`}
-        style={{backgroundColor: '#3b5998'}}
+        style={{borderColor: '#3b5998', color: '#3b5998'}}
       >
         {'Post to FB'}
       </a>
@@ -20,7 +20,7 @@ const Share = ({title, url}: Object) => {
       <a
         className={className}
         href={`https://news.ycombinator.com/submitlink?u=${encodedUrl}&t=${encodedTitle}`}
-        style={{backgroundColor: '#ff6600'}}
+        style={{borderColor: '#ff6600', color: '#ff6600'}}
       >
         {'Post to HN'}
       </a>
@@ -28,7 +28,7 @@ const Share = ({title, url}: Object) => {
       <a
         className={className}
         href={`https://twitter.com/home?status=${encodedUrl}`}
-        style={{backgroundColor: '#0084b4'}}
+        style={{borderColor: '#0084b4', color: '#0084b4'}}
       >
         {'Tweet'}
       </a>
@@ -36,7 +36,7 @@ const Share = ({title, url}: Object) => {
       <a
         className={className}
         href={`mailto:?&subject=You would like this page&body=Check%20out%20${encodedUrl}`}
-        style={{backgroundColor: '#738a8d'}}
+        style={{borderColor: '#738a8d', color: '#738a8d'}}
       >
         {'Send email'}
       </a>
@@ -44,7 +44,7 @@ const Share = ({title, url}: Object) => {
       <a
         className={className}
         href={`https://www.reddit.com/submit?url=${encodedUrl}&title=${encodedTitle}`}
-        style={{backgroundColor: '#FF5700'}}
+        style={{borderColor: '#FF5700', color: '#FF5700'}}
       >
         {'Post to Reddit'}
       </a>
