74906aed5d0978e738f02ad1f12f6aabb842d4f7
Ethan Jon <ethan@Ethans-MacBook-Pro.local>
2017-06-11 14:58:39 -0400

    fb fix

--- react/helpers/link.js
+++ react/helpers/link.js
@@ -2,10 +2,16 @@
 
 import React from 'react'
 import NextLink from 'next/link'
+import URL from 'url'
 
 // by creating our own Link function, we can easily switch from hard <a> links
 // to soft <Link> tags, because of uncertainty in our analytics abilities
 const useHardLinks = false
+
+const relativePath = href => {
+  const urlObj: Object = URL.parse(href)
+  return `${urlObj.pathname}${urlObj.query || ''}`
+}
 
 export default function Link ({children, className, dangerouslySetInnerHTML, href, nextPath}: Object = {}) {
   const aProps: Object = {className}
@@ -26,7 +32,7 @@ export default function Link ({children, className, dangerouslySetInnerHTML, hre
   } else {
     return (
       <NextLink
-        as={href}
+        as={relativePath(href)}
         href={nextPath}
       >
         <a {...aProps} />
