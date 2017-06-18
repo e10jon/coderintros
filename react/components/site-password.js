a4270361181c7ff656e9e7b2e4fb8864ef5fdccb
Ethan Jon <ethan@Ethans-MacBook-Pro.local>
2017-06-18 14:56:44 -0400

    password screen update

--- react/components/site-password.js
+++ react/components/site-password.js
@@ -2,6 +2,7 @@
 
 import React, {Component} from 'react'
 import Head from 'next/head'
+import PropTypes from 'prop-types'
 import URL from 'url'
 
 import {pageXSpacing} from '../helpers/create-page'
@@ -28,30 +29,43 @@ class SitePassword extends Component {
 
         <hr className='mt3 mb3 sm-mb4 ' />
 
-        <div className='center h2 my2 bold'>{'This site requires a password.'}</div>
+        <div
+          className='col-12 mx-auto center my3'
+          style={{maxWidth: '500px'}}
+        >
+          <div className='h2 my2'>{this.context.siteData.description}</div>
+          <div className='h3 my2 gray'>{'Coming soon.'}</div>
 
-        <div className='flex'>
-          <div className='col-10 sm-col-8 md-col-6 mx-auto center'>
-            <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.handleSubmit}>
+            <div
+              className='flex my3 col-12 mx-auto'
+              style={{maxWidth: '350px'}}
+            >
               <input
-                className='center input'
+                className='block input col-10 not-rounded'
                 name='password'
+                placeholder='Enter password to continue...'
                 ref={r => { this.passwordNode = r }}
+                style={{borderRight: 'none'}}
                 type='text'
               />
 
               <button
-                className='btn btn-primary ups regular'
+                className='input regular block col-2 not-rounded btn-primary border-none'
                 type='submit'
               >
                 {'Enter'}
               </button>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
     )
   }
+}
+
+SitePassword.contextTypes = {
+  siteData: PropTypes.object
 }
 
 export default SitePassword
