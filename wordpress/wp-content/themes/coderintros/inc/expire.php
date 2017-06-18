eaf3d050f9c4618e71e5cc33f53505dcff84d5e0
Ethan Jon <ethan@Ethans-MacBook-Pro.local>
2017-06-18 18:11:12 -0400

    expire all caches endpoint

--- wordpress/wp-content/themes/coderintros/inc/expire.php
+++ wordpress/wp-content/themes/coderintros/inc/expire.php
@@ -1,26 +1,18 @@
 <?php
 
-function expire_cache () {
-  // expire our local varnish vache
-  wp_remote_request( get_home_url(), ['method' => 'BAN'] );
+function purge_cloudflare_cache () {
+  $zone_id = get_option( 'cloudflare_zone_id' );
+  $email = get_site_option( 'cloudflare_email' );
+  $api_key = get_site_option( 'cloudflare_api_key' );
 
-  // expire cloudflare cache
-  $cloudflareEmail = get_site_option( 'cloudflare_email' );
-  $cloudflareApiKey = get_site_option( 'cloudflare_api_key' );
-  $cloudflareZoneId = get_option( 'cloudflare_zone_id' );
-
-  if (
-    ! empty( $cloudflareApiKey ) &&
-    ! ( empty( $cloudflareEmail ) ) &&
-    ! ( empty( $cloudflareZoneId ) )
-  ) {
+  if ( !empty( $api_key ) && !empty( $email ) && !empty( $zone_id ) ) {
     wp_remote_request(
-      'https://api.cloudflare.com/client/v4/zones/' . $cloudflareZoneId . '/purge_cache', [
+      'https://api.cloudflare.com/client/v4/zones/' . $zone_id . '/purge_cache', [
         'body' => '{"purge_everything":true}',
         'headers' => [
           'Content-Type' => 'application/json',
-          'X-Auth-Email' => $cloudflareEmail,
-          'X-Auth-Key' => $cloudflareApiKey
+          'X-Auth-Email' => $email,
+          'X-Auth-Key' => $api_key
         ],
         'method' => 'DELETE'
       ]
@@ -28,7 +20,27 @@ function expire_cache () {
   }
 }
 
-add_action( 'save_post', 'expire_cache' );
+function purge_varnish_cache () {
+  wp_remote_request( get_home_url(), ['method' => 'BAN'] );
+}
+
+add_action( 'save_post', 'purge_varnish_cache' );
+add_action( 'save_post', 'purge_cloudflare_cache' );
+
+// add a custom endpoint to expire all site caches
+// useful when called from a post-deploy hook
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'ci', '/expire_all_caches', [
+    'methods' => 'DELETE',
+    'callback' => function () {
+      foreach( get_sites() as $site ) {
+        switch_to_blog( $site->blog_id );
+        purge_cloudflare_cache();
+        purge_varnish_cache();
+      }
+    }
+  ] );
+} );
 
 // TODO: figure out why the following hooks
 // cause a mysql too many connections error:
