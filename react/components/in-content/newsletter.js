8dd36e256f04fadb0cfefce84546be87b18c5bed
Ethan Jon <ethan@Ethans-MacBook-Pro.local>
2017-07-03 13:54:07 -0400

    in-content units

--- react/components/in-content/newsletter.js
+++ react/components/in-content/newsletter.js
@@ -5,10 +5,15 @@ import React from 'react'
 const Newsletter = ({formAction, frequencyGroup}: {formAction: ?string, frequencyGroup: ?string}) => {
   return (
     <div
-      className='sans-serif border border-gray p2 mx-auto bg-darken-0'
-      style={{maxWidth: '450px'}}
+      className='sans-serif border border-gray p2 mx-auto bg-darken-0 bg-cover bg-bottom bg-no-repeat bg-left flex flex-column justify-center bg-gray'
+      style={{
+        height: '280px',
+        maxWidth: '336px'
+      }}
     >
-      <div className='h3 mb3 mt1 line-height-3'>{'Sign up for our newsletter'}</div>
+      <div className='h2 mb2 line-height-3 bold'>
+        {'Get intros in your inbox'}
+      </div>
 
       <form
         action={formAction}
@@ -17,9 +22,9 @@ const Newsletter = ({formAction, frequencyGroup}: {formAction: ?string, frequenc
       >
         <div className='flex'>
           <input
-            className='input mb0 col-9 flex-auto'
+            className='input mb0 col-9 flex-auto not-rounded border-right-none'
             name='EMAIL'
-            placeholder='you@you.com'
+            placeholder='youremail@domain.com'
             type='email'
           />
 
@@ -30,7 +35,7 @@ const Newsletter = ({formAction, frequencyGroup}: {formAction: ?string, frequenc
           />
 
           <button
-            className='btn btn-primary col-3 h5 regular ups'
+            className='btn btn-primary col-3 h5 regular ups not-rounded border-left-none'
             style={{flex: '0 0 80px'}}
             type='submit'
           >
--- react/components/in-content/suggest.js
+++ react/components/in-content/suggest.js
@@ -6,23 +6,28 @@ import Link from 'next/link'
 import {getUrlObj} from '../../helpers/post-data'
 
 const Suggest = () => (
-  <div
-    className='sans-serif border border-gray p2 mx-auto bg-darken-0'
-    style={{maxWidth: '400px'}}
+  <Link
+    as='/suggest'
+    href={getUrlObj({type: 'page', slug: 'suggest'})}
   >
-    <div className='h3 mb3 mt1 line-height-3'>{'Do you know someone who would be great for Coder Intros?'}</div>
+    <a
+      className='block sans-serif border border-gray p2 mx-auto bg-darken-0 bg-cover bg-no-repeat bg-right flex flex-column justify-center'
+      style={{
+        height: '280px',
+        maxWidth: '336px'
+      }}
+    >
+      <div className='h2 mb2 line-height-3 bold'>
+        {'Do you know someone who would be great for Coder Intros?'}
+      </div>
 
-    <div className='mt2'>
-      <Link
-        as='/suggest'
-        href={getUrlObj({type: 'page', slug: 'suggest'})}
-      >
-        <a className='btn btn-primary'>
-          {'Suggest'}
-        </a>
-      </Link>
-    </div>
-  </div>
+      <div>
+        <span className='btn btn-primary'>
+          {'Suggest them'}
+        </span>
+      </div>
+    </a>
+  </Link>
 )
 
 export default Suggest
