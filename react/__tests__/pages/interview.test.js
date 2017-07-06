94021b9a00085ef7107a961f965cd2099e64788a
Ethan Jon <ethan@czahor.com>
2017-07-06 09:41:16 -0400

    testing

--- react/__tests__/pages/interview.test.js
+++ react/__tests__/pages/interview.test.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import {mount} from 'enzyme'
 
-import Interview from '../../pages/interview'
+import ThankYouModal from '../../components/modals/interview/thank-you'
+import WelcomeModal from '../../components/modals/interview/welcome'
+import Post from '../../components/post'
+import Page, {Interview} from '../../pages/interview'
+import PostStore from '../../stores/post'
 
 test('renders', () => {
-  const component = mount(
-    <Interview
+  const page = mount(
+    <Page
+      questionsData={[{section: 'test', questions: ['test']}]}
       siteData={{images: {}}}
     />
   )
-  expect(component.length).toBeTruthy()
+  const component = page.find(Interview)
+
+  expect(page.html().length).toBeTruthy()
+  expect(page.find(Post).length).toBe(1)
+  expect(page.find(WelcomeModal).length).toBe(1)
+  expect(page.find(ThankYouModal).length).toBe(0)
+
+  const store = component.getNode().postStore
+  expect(store).toBeInstanceOf(PostStore)
+  expect(store.questionsData[0].section).toBe('test')
 })
