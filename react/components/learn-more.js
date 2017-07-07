925b120b554cf7db99260a0f94023e7f1e21ca88
Ethan Jon <ethan@czahor.com>
2017-07-07 09:55:09 -0400

    learn more styling

--- react/components/learn-more.js
+++ react/components/learn-more.js
@@ -8,60 +8,76 @@ import {
   IoSocialTwitter as TwitterIcon
 } from 'react-icons/lib/io'
 
-const LearnMore = ({postData}: Object) => (
-  <div className='h4'>
-    <div className='h3 muted mb2 mt1 line-height-3'>{`Learn more about ${postData.name}`}</div>
+const createPrettyUrl = (url: string) => url
+  .replace(/^https?:\/\/(?:www\.)?/i, '')
+  .replace(/\/$/, '')
 
-    <div>
-      {postData.facebook_url && (
-        <a
-          className='inline-block gray mr2 pb1'
-          href={postData.facebook_url}
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <FacebookIcon />
-          <span className='px1 align-middle h5'>{postData.facebook_url}</span>
-        </a>
-      )}
+const LearnMore = ({postData}: Object) => {
+  const aClassName = 'block sm-inline-block gray pb1 mr2'
 
-      {postData.linkedin_url && (
-        <a
-          className='inline-block gray mr2 pb1'
-          href={postData.linkedin_url}
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <LinkedinIcon />
-          <span className='px1 align-middle h5'>{postData.linkedin_url}</span>
-        </a>
-      )}
+  return (
+    <div className='h4'>
+      <div className='h3 muted mb2 mt1 line-height-3'>{`Learn more about ${postData.name}`}</div>
 
-      {postData.personal_url && (
-        <a
-          className='inline-block gray mr2 pb1'
-          href={postData.personal_url}
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <PersonalIcon />
-          <span className='px1 align-middle h5'>{postData.personal_url}</span>
-        </a>
-      )}
+      <div>
+        {postData.facebook_url && (
+          <a
+            className={aClassName}
+            href={postData.facebook_url}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <FacebookIcon />
+            <span className='px1 align-middle h5'>
+              {createPrettyUrl(postData.facebook_url)}
+            </span>
+          </a>
+        )}
 
-      {postData.twitter_url && (
-        <a
-          className='inline-block gray mr2 pb1'
-          href={postData.twitter_url}
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          <TwitterIcon />
-          <span className='px1 align-middle h5'>{postData.twitter_url}</span>
-        </a>
-      )}
+        {postData.linkedin_url && (
+          <a
+            className={aClassName}
+            href={postData.linkedin_url}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <LinkedinIcon />
+            <span className='px1 align-middle h5'>
+              {createPrettyUrl(postData.linkedin_url)}
+            </span>
+          </a>
+        )}
+
+        {postData.personal_url && (
+          <a
+            className={aClassName}
+            href={postData.personal_url}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <PersonalIcon />
+            <span className='px1 align-middle h5'>
+              {createPrettyUrl(postData.personal_url)}
+            </span>
+          </a>
+        )}
+
+        {postData.twitter_url && (
+          <a
+            className={aClassName}
+            href={postData.twitter_url}
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <TwitterIcon />
+            <span className='px1 align-middle h5'>
+              {createPrettyUrl(postData.twitter_url)}
+            </span>
+          </a>
+        )}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default LearnMore
