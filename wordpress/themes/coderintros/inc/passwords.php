c8e85996596d72907034403e399b75341accd48f
Ethan Jon <ethan@czahor.com>
2017-07-16 08:34:14 -0400

    password feed fix

--- wordpress/themes/coderintros/inc/passwords.php
+++ wordpress/themes/coderintros/inc/passwords.php
@@ -5,7 +5,7 @@
 // we'll still need to manually filter for REST requests
 // https://codex.wordpress.org/Using_Password_Protection
 add_action( 'pre_get_posts', function ( $query ) {
-  if ( !is_single() && !is_page() && !is_admin() && !REST_REQUEST ) {
+  if ( !is_single() && !is_page() && !is_admin() && !defined( 'REST_REQUEST' ) ) {
     add_filter( 'posts_where', function ( $where ) {
       global $wpdb;
       return $where .= " AND {$wpdb->posts}.post_password = '' ";
