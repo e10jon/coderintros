9270bb6bd1a4494cb15c637b3fa69ee1281a55bb
Ethan Jon <ethan@czahor.com>
2017-07-26 07:05:30 -0400

    added facebook pixel conversion

--- react/components/modals/interview/thank-you.js
+++ react/components/modals/interview/thank-you.js
@@ -1,30 +1,40 @@
 // @flow
 
-import React from 'react'
+import React, {PureComponent} from 'react'
 import Link from 'next/link'
 
 import createModal from '../../../helpers/create-modal'
 import {getUrlObj} from '../../../helpers/post-data'
 
-const ThankYou = () => (
-  <div className='p3 bg-darken-0'>
-    <div className='h2 bold line-height-3 mb2'>{'You\'re good to go!'}</div>
+class ThankYou extends PureComponent {
+  componentDidMount () {
+    if (window.fbq) {
+      window.fbq('track', 'CompleteRegistration')
+    }
+  }
 
-    <p>{'Thank you so much for your submission. No further action is required from you at this point.'}</p>
-    <p>{'We will review and lightly edit your submission soon. We may contact you to gather additional information.'}</p>
-    <p>{'Once your intro is published and ready to be shared, we\'ll send you an email.'}</p>
-    <p>{'Now, would you be so kind as to spread the word and suggest someone you know?'}</p>
+  render () {
+    return (
+      <div className='p3 bg-darken-0'>
+        <div className='h2 bold line-height-3 mb2'>{'You\'re good to go!'}</div>
 
-    <div className='mt2'>
-      <Link
-        as='/suggest'
-        href={getUrlObj({slug: 'suggest', type: 'page'})}
-      >
-        <a className='btn btn-primary'>{'Suggest someone'}</a>
-      </Link>
-    </div>
-  </div>
-)
+        <p>{'Thank you so much for your submission. No further action is required from you at this point.'}</p>
+        <p>{'We will review and lightly edit your submission soon. We may contact you to gather additional information.'}</p>
+        <p>{'Once your intro is published and ready to be shared, we\'ll send you an email.'}</p>
+        <p>{'Now, would you be so kind as to spread the word and suggest someone you know?'}</p>
+
+        <div className='mt2'>
+          <Link
+            as='/suggest'
+            href={getUrlObj({slug: 'suggest', type: 'page'})}
+          >
+            <a className='btn btn-primary'>{'Suggest someone'}</a>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default createModal(ThankYou, {
   hideCloseButton: true,
